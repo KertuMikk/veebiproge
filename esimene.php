272c1616fda45b4348de2521d3437e79aacb5b03
Kertu Mikk <KertuMikk@users.noreply.github.com>
2017-09-11 13:50:05 +0300

    esimene PHP fail HTMLiga

---
+++ esimene.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8">
+   <title>Throney</title>
+
+<link rel="icon" href="https://static.digitecgalaxus.ch/Files/8/1/8/0/7/4/6/person_image_286537.gif" type="GIF">
+
+</head>
+<body background="https://cloud.graphicleftovers.com/51202/1515286/back.jpg">
+<header>  
+ <h1> <font face="Comic Sans MS"
+              size="10"
+              color= "Ivory"> 
+Throney </font> </h1>
+</header>
+
+   <p> <font face="Comic Sans MS" 
+             size="3" 
+             color= "Ivory">
+ See veebileht on loodud õppetöö raames <br> 
+ ja ei sisalda tõsiseltvõetavat sisu! <br> </font>
+ </p>
+
+
+<p>
+<img src="https://media.giphy.com/media/RX3vhj311HKLe/giphy.gif" 
+alt="nopower" height="242" width="350"> 
+</p>
+<p>
+<img src="https://media.giphy.com/media/LXP19BrVaOOgE/giphy.gif" 
+alt="RamseyBolton" height="242" width="350"> 
+</p>
+<p>
+<img src="https://media.giphy.com/media/Cr7yTbjNuY27C/giphy.gif" 
+alt="gollum" height="242" width="350"> 
+</p>
+<p>
+<iframe width="560" height="315" 
+src="https://www.youtube.com/embed/1lPFfnrzIHM" frameborder="0" 
+</iframe>
+</p>
+
+</body>
+</html>
+
+
+
