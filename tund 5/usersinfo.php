3b7d426677ffe10690c8c43f578b6d025c504b4c
Kertu Mikk <KertuMikk@users.noreply.github.com>
2017-10-03 17:48:41 +0300

    login vormi täiustamine, sisse ja välja logimise lisamine

---
+++ tund 5/usersinfo.php
@@ -0,0 +1,101 @@
+<?php
+
+	require("functions.php");
+
+	//kui pole sisse logitud, liigume login lehele
+
+	if(!isset($_SESSION["userId"])){
+
+		header("Location: login.php");
+
+		exit();
+
+	}
+
+	
+
+	//väljalogimine
+
+	if(isset($_GET["logout"])){
+
+		session_destroy(); //lõpetab sessiooni
+
+		header("Location: login.php");
+
+	}
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>
+
+		Throney
+
+	</title>
+
+</head>
+
+<body background="https://www.muralswallpaper.co.uk/app/uploads/pattern-clouds-pink-and-grey-nursery-plain-820x532.jpg">
+
+	
+
+	<p>There is absolutely nothing serious here</p>
+
+	<p><a href="?logout=1">Log out</a></p>
+
+	<p><a href="main.php">Main page</a></p>
+
+	
+
+	<table border="1" style="border-collapse: collapse;">
+
+		<tr>
+
+			<th>First name</th>
+
+			<th>Last name</th>
+
+			<th>username</th>
+
+		</tr>
+
+		<tr>
+
+			<td>Kertu</td>
+
+			<td>Mikk</td>
+
+			<td>kertumik@tlu.ee</td>
+
+		</tr>
+
+		<tr>
+
+			<td>Mari</td>
+
+			<td>Karus</td>
+
+			<td>kasrusmari@aed.ee</td>
+
+		</tr>
+
+	
+
+	</table>
+
+	
+
+</body>
+
+</html>
